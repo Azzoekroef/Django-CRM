2532afcabb27d1f8b1c3b059c6bc29775bde1b0d
Azzoekroef <151991631+Azzoekroef@users.noreply.github.com>
2025-01-20 02:15:17 +0700

    Update settings.py

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'elderco',
         'USER': 'root',
-        'PASSWORD': "Aspire.1234",
+        'PASSWORD': "",
         'HOST': 'localhost',
         'PORT': '3306',
     }
